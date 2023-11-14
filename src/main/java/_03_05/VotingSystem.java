641fdd78ef2b5d3a5d77d6b818efaa9fcfaba0f3
Serghei17 <serghei7777@gmail.com>
2023-11-14 16:26:31 +0200

    LinkedHashMap challenge

--- src/main/java/_03_05/VotingSystem.java
+++ src/main/java/_03_05/VotingSystem.java
@@ -4,19 +4,19 @@ import java.util.LinkedHashMap;
 
 public class VotingSystem {
 
-    // Create a LinkedHashMap here, with the contestant name as the key
-    // and the number of votes as the value.
+  // Create a LinkedHashMap here, with the contestant name as the key
+  // and the number of votes as the value.
+  private LinkedHashMap<String, Integer> contestantScores = new LinkedHashMap<>();
 
-    void vote(String contestant) {
-        // Implement this method so that a new contestant is added to the map if it's
-        // not already there. If it is already there, increment the value by one.
-    }
+  void vote(String contestant) {
+    // Implement this method so that a new contestant is added to the map if it's
+    // not already there. If it is already there, increment the value by one.
+    contestantScores.merge(contestant, 1, Integer::sum);
+  }
 
-    // This method should return the LinkedHashMap field you create at the top of
-    // the class.
-    public LinkedHashMap getVotes() {
-        return new LinkedHashMap();
-    }
-
-
+  // This method should return the LinkedHashMap field you create at the top of
+  // the class.
+  public LinkedHashMap<String, Integer> getVotes() {
+    return contestantScores;
+  }
 }
